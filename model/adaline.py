57390aebd80de16cb0e7a638b46ceb9809beff34
Arthur Layese <arthur@layese.com>
2020-12-12 09:39:30 +0800

    added adaline

--- model/adaline.py
+++ model/adaline.py
@@ -1,60 +1,30 @@
-import numpy as np
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from random import random
 from model import Neuron
+import numpy as np
 
 class Adaline(Neuron):
-  def __init__(self, n_iterations=100, random_state=1, learning_rate=0.001):
-    self.n_iterations = n_iterations
-    self.random_state = random_state
-    self.learning_rate = learning_rate
-
-  """
-    desired output - sum of error
-    computation of total error is not absolute but rather LMSE (least means square error)
-    square of sum of error divided by 2
-    change total error to LMSE
-    for every epoch accummulated LMSE divided input pattern
-    compare it with threshold
-    threshold = (0.01 or 0.001)
-    while (lmse > 0.01 and counter < epoch) continue training
-  """
   def fit(self, X, y):
-    self.weights = [random() for i in range(len(X[0])+1)] # include bias
+    X = self.standardize_features(X)
+    X = self.add_bias(X)
+    self.init_weights(X)
     lmse = 0
-    for epoch in range(self.n_iterations):
+    for epoch in range(self.iterations):
       errors = y - self.activation_function(self.net_input(X))
-      weights = self.weights[1:]
-      all_errors = self.dot(self.transpose(X), errors)
-      for i in range(len(weights)):
-        self.weights[i+1] += self.learning_rate * all_errors[i]
-      self.weights[0] += self.learning_rate * self.sum_errors(errors)
-      lmse = self.sum_errors(errors**2) / 2.0
-      if (lmse <= self.learning_rate):
-        print(lmse)
+      self.weights += self.learning_rate * X.T.dot(errors)
+      #lmse = (errors ** 2).sum() / 2
+      lmse = self.sum_squared_errors(errors)
+      #print(errors)
+      if lmse <= self.learning_rate:
         break
     return self.weights, epoch
 
-  def sum_errors(self, errors):
-    total = 0
-    for error in errors:
-      total += error
-    return total
-
-  def transpose(self, X):
-    return list(map(list, zip(*X)))
-
-  def net_input(self, X):
-    weights = self.weights[1:]
-    # 8.961276131504245e+58
-    output = np.dot(X, weights) + self.weights[0]
-    #print('output: ', output)
-    return np.dot(X, self.weights[1:]) + self.weights[0]
-
-  # identify function
-  def activation_function(self, X):
-    return X
+  def sum_squared_errors(self, errors):
+    return (errors ** 2).sum() / 2
 
   def predict(self, X):
     return 1 if self.activation_function(self.net_input(X)) >= 0.0 else 0
+
+  def activation_function(self, X):
+    return X
+
+  def standardize_features(self, X):
+    return (X - np.mean(X, axis=0)) / np.std(X, axis=0)
